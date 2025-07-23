3cb7abe1b84f1b14b45a80c567fd2c0432129df7
Dmitry Zankov <dvzankov@gmail.com>
2025-07-23 11:40:13 +0200

    conformer generation verbose added

--- qsarmil/conformer/rdkit.py
+++ qsarmil/conformer/rdkit.py
@@ -5,8 +5,8 @@ from qsarmil.conformer.base import ConformerGenerator
 
 
 class RDKitConformerGenerator(ConformerGenerator):
-    def __init__(self, num_conf=10, e_thresh=None, num_cpu=1):
-        super().__init__(num_conf=num_conf, e_thresh=e_thresh, num_cpu=num_cpu)
+    def __init__(self, num_conf=10, e_thresh=None, num_cpu=1, verbose=True):
+        super().__init__(num_conf=num_conf, e_thresh=e_thresh, num_cpu=num_cpu, verbose=verbose)
 
     def _prepare_molecule(self, mol):
         mol = Chem.AddHs(mol)
